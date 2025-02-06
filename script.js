2e62c2375a047b69f97fcdf2d6b44eada4886b3f
Koruji <jadecottin212@gmail.com>
2025-02-06 14:00:57 +0100

    vercel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://localhost:3000/';
+const BACKEND_URL = 'ticket-hack-backend-three.vercel.app/';
 const resultsSection = document.querySelector('.results-section');
 const searchForm = document.querySelector("#search-form");
 
